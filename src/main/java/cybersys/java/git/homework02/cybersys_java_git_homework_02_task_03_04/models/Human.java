07fefaedcb6d464717bd45b46d7be64a0d3a78f0
Sarsenov <zhanbek.sarsenov@gmail.com>
2025-09-16 11:28:09 +0500

    Завдання 7 Створіть нові версії файлів.

--- src/main/java/cybersys/java/git/homework02/cybersys_java_git_homework_02_task_03_04/models/Human.java
+++ src/main/java/cybersys/java/git/homework02/cybersys_java_git_homework_02_task_03_04/models/Human.java
@@ -1,4 +1,5 @@
 package cybersys.java.git.homework02.cybersys_java_git_homework_02_task_03_04.models;
 
 public interface Human {
+	void speak();
 }
--- src/main/java/cybersys/java/git/homework02/cybersys_java_git_homework_02_task_03_04/models/Student.java
+++ src/main/java/cybersys/java/git/homework02/cybersys_java_git_homework_02_task_03_04/models/Student.java
@@ -1,4 +1,7 @@
 package cybersys.java.git.homework02.cybersys_java_git_homework_02_task_03_04.models;
 
-public class Student {
+public class Student implements Human {
+	void speak() {
+		System.out.println("Bla-bla-bla");
+	}
 }
