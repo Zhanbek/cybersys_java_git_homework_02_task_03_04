224a97a61797cd60bb4cf0bdd235ffa2e8d85cd6
Sarsenov <zhanbek.sarsenov@gmail.com>
2025-09-16 09:31:49 +0500

    add project

---
+++ src/main/java/cybersys/java/git/homework02/cybersys_java_git_homework_02_task_03_04/CybersysJavaGitHomework02Task0304Application.java
@@ -0,0 +1,13 @@
+package cybersys.java.git.homework02.cybersys_java_git_homework_02_task_03_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybersysJavaGitHomework02Task0304Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CybersysJavaGitHomework02Task0304Application.class, args);
+    }
+
+}
---
+++ src/test/java/cybersys/java/git/homework02/cybersys_java_git_homework_02_task_03_04/CybersysJavaGitHomework02Task0304ApplicationTests.java
@@ -0,0 +1,13 @@
+package cybersys.java.git.homework02.cybersys_java_git_homework_02_task_03_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybersysJavaGitHomework02Task0304ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
